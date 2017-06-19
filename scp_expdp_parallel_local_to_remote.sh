ead5343ff61c677beed012711c64fed4e7d76ff3
guestart <guestart@163.com>
2017-06-19 14:59:55 +0800

    Update scp_expdp_parallel_local_to_remote.sh

--- scp_expdp_parallel_local_to_remote.sh
+++ scp_expdp_parallel_local_to_remote.sh
@@ -5,10 +5,9 @@
 # |                            guestart@163.com                            |
 # |                        guestart.blog.51cto.com                         |
 # |------------------------------------------------------------------------|
-# |      Copyright (c) 2016-2017 Quanwen Zhao. All rights reservered.      |
+# |       Copyright (c) 2016-2017 Quanwen Zhao. All rights reserved.       |
 # |------------------------------------------------------------------------|
 # | DATABASE   : Oracle                                                    |
-# | OS ENV     : CentOS 6.6 X86_64 Bit                                     |
 # | FILE       : scp_expdp_parallel_local_to_remote.sh                     |
 # | CLASS      : LINUX Bourne-Again Shell Scripts                          |
 # | PURPOSE    : This bash script file used to copy several files that     |
@@ -25,7 +24,7 @@
 # |                                                                        |
 # | PARAMETERS : None.                                                     |
 # |                                                                        |
-# | MODIFIED   : 03/17/2017 (mm/dd/yyyy)                                   |
+# | MODIFIED   : 03/17/2017 (dd/mm/yyyy)                                   |
 # |                                                                        |
 # | NOTE       : As with any code, ensure to test this script in a         |
 # |              development environment before attempting to run it in    |
@@ -36,7 +35,7 @@
 # | EXPORT ENVIRONMENT VARIABLE OF ORACLE USER                             |
 # +------------------------------------------------------------------------+
 
-source ~/.bash_profile;
+source ~/.bash_profile
 
 # +------------------------------------------------------------------------+
 # | GLOBAL VARIABLES ABOUT THE ABSOLUTE PATH OF THE SHELL COMMAND          |
@@ -55,13 +54,14 @@ export RM=`which rm`
 export SCP=`which scp`
 export SCRIPT=`which script`
 export SSH=`which ssh`
+export TEE=`which tee`
 export UNIQ=`which uniq`
 
 # +------------------------------------------------------------------------+
 # | SWITCH CURRENT DIRECTORY TO EXPDP DIRECTORY OF ORACLE USER WITH CD     |
 # +------------------------------------------------------------------------+
 
-cd /u01/oradata/rman_back/expdp
+cd /u01/oradata/rman_backup/expdp
 
 # +------------------------------------------------------------------------+
 # | GLOBAL VARIABLES ABOUT THE BACKTICK EXECUTION RESULT OF THE SHELL      |
@@ -75,8 +75,9 @@ export EXPDP_TOTAL_SIZE_M=`$DU -m *\`$DATE +%Y%m%d\`* | $CUT -f1 | $AWK '{sum +=
 # | GLOBAL VARIABLES ABOUT STRINGS AND ABSOLUTE PATH OF THE SHELL COMMAND  |
 # +------------------------------------------------------------------------+
 
-export DISK_ARRAY='HDA HDB HDC HDD HDE HDF HDG HDH HDI HDJ HDK HDL HDM HDN HDO HDP'
-export SCP_LOG_LOCATION='/u01/oradata/rman_back/scp_expdp_log'
+export DISK_ARRAY='HDC HDD HDE HDF HDG HDH HDI HDJ HDK HDL HDM HDN'
+export SCP_LOG_PATH='/u01/oradata01/rman_backup/scp_expdp_base_log'
+export SCP_LOG_FILE=$SCP_LOG_PATH/scp_expdp_base_`$DATE +%Y%m%d%H%M%S`.log
 
 # +------------------------------------------------------------------------+
 # | GLOBAL FUNCTIONS                                                       |
@@ -84,24 +85,25 @@ export SCP_LOG_LOCATION='/u01/oradata/rman_back/scp_expdp_log'
 
 function scp_expdp_parallel () {
 
-    for i in $DISK_ARRAY;
+  for i in $DISK_ARRAY;
+  do
+    AVAIL_DISK_SIZE_M=`$SSH oracle@10.103.0.22 $DF -m | $GREP $i | $AWK '{print $4}'`
+    if [ `$EXPR $AVAIL_DISK_SIZE_M / 1024` -gt `$EXPR $EXPDP_TOTAL_SIZE_M / 1024` ]; then
+    for j in $EXPDP_DMP;
     do
-      AVAIL_DISK_SIZE_M=`$SSH oracle@172.16.20.22 $DF -m | $GREP $i | $AWK '{print $4}'`
-      if [ `$EXPR $AVAIL_DISK_SIZE_M / 1024` -gt `$EXPR $EXPDP_TOTAL_SIZE_M / 1024` ]; then
-        for j in $EXPDP_DMP;
-        do
-          EXPDP_DATE=`$ECHO $j | $CUT -c 1-8`
-          if [ $EXPDP_DATE -eq $CURRENT_DATE ]; then
-            $SCRIPT -a $SCP_LOG_LOCATION/scp_expdp_`date +%Y%m%d%H%M%S`.log -c "$SCP -p expdp_$j* oracle@172.16.20.22:/$i"
-          else
-            $RM -rf expdp_$j*
-          fi
-        done
-        exit;
-      else
-        continue;
+      EXPDP_DATE=`$ECHO $j | $CUT -c 1-8`
+      if [ $EXPDP_DATE -eq $CURRENT_DATE ]; then
+        $SCRIPT -a $SCP_LOG_FILE -c "$SCP -p expdp_szd_base_v2_$j* oracle@10.103.0.22:/$i"
+        if [ `$ECHO $?` -ne 0 ]; then
+          $ECHO -e "\nRemote copy all of dmp and log files that are generated today failed.Please check error information from log file.\n" | $TEE -a $SCP_LOG_FILE
+        fi
       fi
-done
+    done
+    exit;
+    else
+      continue;
+    fi
+  done
 
 }
 
