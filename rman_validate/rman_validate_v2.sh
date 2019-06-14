07901fb26d311af8153b11b32424768bcd593c2b
Quanwen Zhao <guestart@163.com>
2019-06-14 11:20:17 +0800

    Update rman_validate_v2.sh

--- rman_validate/rman_validate_v2.sh
+++ rman_validate/rman_validate_v2.sh
@@ -1,57 +1,89 @@
 #!/bin/bash
 
 # +-----------------------------------------------------------------------+
+# |                                                                       |
 # |                              Quanwen Zhao                             |
+# |                                                                       |
 # |                            guestart@163.com                           |
+# |                                                                       |
 # |                       quanwenzhao.wordpress.com                       |
+# |                                                                       |
 # |-----------------------------------------------------------------------|
+# |                                                                       |
 # |      Copyright (c) 2016-2017 Quanwen Zhao. All rights reserved.       |
+# |                                                                       |
 # |-----------------------------------------------------------------------|
-# | DATABASE   : Oracle                                                   |
-# | OS ENV     : CentOS 6.6 X86_64 Bit                                    |
-# | File       : rman_validate_v2.sh                                      |
-# | CLASS      : LINUX Bourne-Again Shell Scripts                         |
-# | PURPOSE    : This bash script file used to validate rman backupset    |
-# |              that is generated last night via validate command on     |
-# |              Oracle Database Server.                                  |
 # |                                                                       |
-# | PARAMETERS : None.                                                    |
+# |  DATABASE   : Oracle                                                  |
 # |                                                                       |
-# | MODIFIED   : 03/17/2017 (mm/dd/yyyy)                                  |
+# |  OS ENV     : CentOS 6.6 X86_64 Bit                                   |
 # |                                                                       |
-# | NOTE       : As with any code, ensure to test this script in a        |
-# |              development environment before attempting to run it in   |
-# |              production.                                              |
+# |  File       : rman_validate_v2.sh                                     |
+# |                                                                       |
+# |  CLASS      : LINUX Bourne-Again Shell Scripts                        |
+# |                                                                       |
+# |  PURPOSE    : This bash script file used to validate rman backupset   |
+# |                                                                       |
+# |               that is generated last night via validate command on    |
+# |                                                                       |
+# |               Oracle Database Server.                                 |
+# |                                                                       |
+# |  PARAMETERS : None.                                                   |
+# |                                                                       |
+# |  MODIFIED   : 17/03/2017 (dd/mm/yyyy)                                 |
+# |                                                                       |
+# |  NOTE       : As with any code, ensure to test this script in a       |
+# |                                                                       |
+# |               development environment before attempting to run it in  |
+# |                                                                       |
+# |               production.                                             |
+# |                                                                       |
 # +-----------------------------------------------------------------------+
 
 # +-----------------------------------------------------------------------+
+# |                                                                       |
 # | EXPORT ENVIRONMENT VARIABLE OF ORACLE USER                            |
+# |                                                                       |
 # +-----------------------------------------------------------------------+
 
 source ~/.bash_profile;
 
 # +-----------------------------------------------------------------------+
+# |                                                                       |
 # | GLOBAL VARIABLES ABOUT THE ABSOLUTE PATH OF THE SHELL COMMAND         |
+# |                                                                       |
 # +-----------------------------------------------------------------------+
 
 export AWK=`which awk`
+
 export DATE=`which date`
+
 export ECHO=`which echo`
 
 # +-----------------------------------------------------------------------+
+# |                                                                       |
 # | GLOBAL VARIABLES ABOUT STRINGS AND BACKTICK EXECUTION RESULT OF SHELL |
+# |                                                                       |
 # +-----------------------------------------------------------------------+
 
 export BACK_LOG=~/rman_backup/log
+
 export RMAN=$ORACLE_HOME/bin/rman
+
 export SQLPLUS=$ORACLE_HOME/bin/sqlplus
+
 export YESTERDAY=`$DATE +%Y-%m-%d -d yesterday`
+
 export DAY_OF_WEEK=`$DATE +%u`
+
 export BSKEY_LIST=
+
 export BSKEY_LIST_WITH_COMMA=
 
 # +-----------------------------------------------------------------------+
+# |                                                                       |
 # | QUERY ALL OF BS_KEY VALUE OF RMAN BACKUPSET YESTERDAY INTO BSKEY_LIST |
+# |                                                                       |
 # +-----------------------------------------------------------------------+
 
 BSKEY_LIST=`
@@ -63,13 +95,17 @@ exit;
 EOF`
 
 # +-----------------------------------------------------------------------+
+# |                                                                       |
 # | WITH AWK COMMAND TO PROCESS BSKEY_LIST SAVE TO BSKEY_LIST_WITH_COMMA  |
+# |                                                                       |
 # +-----------------------------------------------------------------------+
 
 BSKEY_LIST_WITH_COMMA=`$ECHO $BSKEY_LIST | $AWK -F' ' '{ for ( i=1; i<NF; i++ ) print $i","; print $NF }'`
 
 # +-----------------------------------------------------------------------+
+# |                                                                       |
 # | VALIDATE RMAN BACKUPSET THAT IS GENERATED LAST NIGHT                  |
+# |                                                                       |
 # +-----------------------------------------------------------------------+
 
 case $DAY_OF_WEEK in
