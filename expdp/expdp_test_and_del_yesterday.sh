c88e961dc23a76f624064bbe22f26357e7fe2d58
Quanwen Zhao <guestart@163.com>
2019-07-05 16:49:45 +0800

    Update expdp_test_and_del_yesterday.sh

--- expdp/expdp_test_and_del_yesterday.sh
+++ expdp/expdp_test_and_del_yesterday.sh
@@ -1,3 +1,6 @@
+# Author:        Quanwen Zhao
+# Last Updated:  Jul 05, 2019
+
 #!/bin/bash
 
 # The crontab setting on oracle user of linux server is as follows:
@@ -28,7 +31,25 @@ export CURRENT_DATE=`$DATE +%Y%m%d%H%M%S`
 # Export and generate dmp and log file of Oracle user about test
 # via expdp command
 
-$EXPDP test/xxxx@test schemas=test directory=expdp parallel=3 dumpfile=expdp_test_${CURRENT_DATE}_%U.dmp filesize=50G logfile=expdp_test_${CURRENT_DATE}.log statistics=none indexes=y
+# $EXPDP test/xxxx@test schemas=test directory=expdp parallel=3 dumpfile=expdp_test_${CURRENT_DATE}_%U.dmp filesize=50G logfile=expdp_test_${CURRENT_DATE}.log statistics=none indexes=y
+# If using the above expdp command as you can see the below export message warnings.
+
+# Export: Release 11.2.0.4.0 - Production on Fri Jul 5 14:28:51 2019
+
+# Copyright (c) 1982, 2011, Oracle and/or its affiliates.  All rights reserved.
+
+# Connected to: Oracle Database 11g Enterprise Edition Release 11.2.0.4.0 - 64bit Production
+# With the Partitioning, Oracle Label Security, OLAP, Data Mining,
+# Oracle Database Vault and Real Application Testing options
+# Legacy Mode Active due to the following parameters:
+# Legacy Mode Parameter: "indexes=FALSE" Location: Command Line, Replaced with: "exclude=index"  <<==
+# Legacy Mode Parameter: "statistics=none" Location: Command Line, ignored.  <<==
+# Legacy Mode has set reuse_dumpfiles=true parameter.  <<==
+# ......
+
+# Use the following right expdp command to exclude both statistics and index,
+# because of the previous expdp command still export the statistics info and index.
+$EXPDP test/xxxx@test schemas=test directory=expdp parallel=3 dumpfile=expdp_test_${CURRENT_DATE}_%U.dmp filesize=50G logfile=expdp_test_${CURRENT_DATE}.log exclude=statistics, index
 
 ##########################################################
 
